ddb6947df454ea9edb77bde1a6d8a0fdfd84774f
darkfantasy88 <59861596+darkfantasy88@users.noreply.github.com>
2020-02-23 14:32:30 -0500

    Start of Library To Manage ORM and Dependecy Injection

---
+++ ALT+R_Solutions/ALT+R_ManagerLibrary/DataAccess/Dependency_Inversion_Principle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+namespace ALT_R_ManagerLibrary.DataAccess
+{
+    public class Dependency_Inversion_Principle:Module
+    {
+        ContainerBuilder builder;
+        IContainer container;
+        public Dependency_Inversion_Principle()
+        {
+             builder= new ContainerBuilder();
+            
+        }
+        public void BuildContainer()
+        {
+            container=builder.Build();
+        }
+
+        public void RegisterInstance<T,U>(T _interface,U _implementation)
+        {
+            builder.RegisterType<U>().As<T>();
+        }
+
+        public T GetInstanceOf<T>()
+        {
+            var scope = container.BeginLifetimeScope();
+            var instance_=scope.Resolve<T>();
+            return instance_;
+        }
+    }
+}
---
+++ ALT+R_Solutions/ALT+R_ManagerLibrary/Internal Data Acess/SqlDataAccess.cs
@@ -0,0 +1,41 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALT_R_ManagerLibrary.Internal_Data_Acess
+{
+    internal class SqlDataAccess
+    {
+        private string GetConnectStringFromConfig(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+
+        public IEnumerable<T> RetrieveData<T,U>(string storedProcedure,U paramaters,string connectionName)
+        {
+            var connectionString = GetConnectStringFromConfig(connectionName);
+
+            using (IDbConnection dbConnection=new SqlConnection(connectionString))
+            {
+                var rows = dbConnection.Query<T>(storedProcedure, paramaters, commandType: CommandType.StoredProcedure).ToList();
+                return rows;
+            }
+        }
+
+        public async void SaveData(string storedProcedure,string connectionName)
+        {
+            var connectionString = GetConnectStringFromConfig(connectionName);
+
+            using (IDbConnection dbConnection = new SqlConnection(connectionString))
+            {
+                await dbConnection.ExecuteAsync(storedProcedure,commandType: CommandType.StoredProcedure);                
+            }
+        }
+    }
+}
